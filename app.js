ea3ee55a24c5b1949f4a66d3eb2834d8bac48ad9
Ravi Bisht <ravi2bisht@gmail.com>
2022-01-06 23:59:49 +0530

    Security fetures added

---
+++ app.js
@@ -0,0 +1,49 @@
+import express, { urlencoded } from "express"
+import 'dotenv/config'
+import authRouter from './routes/auth'
+import authenticationMiddleWare from "./middleware/authenticationMiddleWare"
+import jobsRouter from './routes/jobs'
+import connect from "./db/connect"
+import { HttpException } from "./exception"
+import errorHandlerMiddleWare from "./middleware/errorHandlerMiddleWare"
+import notFoundMiddleware from "./middleware/notFoundMiddleware"
+import helmet from "helmet"
+import rateLimit from "express-rate-limit"
+import xssClean from "xss-clean"
+import cors from "cors"
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// App Config
+app.use(urlencoded({ extended: true }))
+app.use(express.static('./public'))
+app.use(express.json())
+
+// Security
+app.use(rateLimit({
+    windowMs: 15 * 60 * 100, // 15 miniute
+    max: 100
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xssClean())
+
+// Routes
+app.use(process.env.BASE_SLUG, authRouter)
+app.use(`${process.env.BASE_SLUG}/job`, authenticationMiddleWare, jobsRouter)
+app.use(errorHandlerMiddleWare)
+app.use(notFoundMiddleware)
+
+
+
+const start = async () => {
+    try {
+        await connect(process.env.MONGO_URI)
+        app.listen(PORT, () => console.log(`Server is running in port no : ${PORT}`))
+    } catch (error) {
+        console.error(error);
+    }
+}
+start()
--- middleware/errorHandlerMiddleWare.js
+++ middleware/errorHandlerMiddleWare.js
@@ -2,11 +2,41 @@ import { StatusCodes } from "http-status-codes"
 import { HttpException } from "../exception"
 
 export const errorHandlerMiddleWare = (err, req, res, next) => {
+
+
     if (err instanceof HttpException) {
         return res.status(err.statusCode).json({ message: err.message })
     }
 
-    return res.status(301).json({ message: err })
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        message: err.message || "Something went wrong."
+    }
+
+    if (err.code && err.code == 11000) {
+        console.log(err.keys);
+        customError.message = `Duplicate value entered for ${Object.keys(err.keyValue || "")} field please choose another value.`
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+    if (err.code && err.code == 11000) {
+        console.log(err.keys);
+        customError.message = `Duplicate value entered for ${Object.keys(err.keyValue || "")} field please choose another value.`
+        customError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+    if (err.name && err.name === "CastError") {
+        customError.message = `No item with id : ${err.value}`
+        customError.statusCode = StatusCodes.NOT_FOUND
+    }
+
+    if (err.name === "ValidationError") {
+        customError.message = Object.values(err.errors).map((item) => item.message).join(',')
+        customError.statusCode = StatusCodes.NOT_FOUND
+    }
+
+
+    return res.status(customError.statusCode).json({ message: customError.message })
 }
 
 export default errorHandlerMiddleWare 
