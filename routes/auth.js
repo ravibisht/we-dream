0782e5d344019c98af36b9515c4891d4271761a2
Ravi Bisht <ravi2bisht@gmail.com>
2022-01-08 02:28:37 +0530

    Fix bugs

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+import express from "express"
+import { login, register } from '../controller/auth'
+
+const router = express.Router()
+
+router.route('/login').post(login)
+router.route('/register').post(register)
+export default router
+
+
